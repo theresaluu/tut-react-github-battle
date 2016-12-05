5a772723d53b41f6ddb36763b40f75cd24c730e1
Theresa Luu <theresa.luu@gmail.com>
2016-12-04 23:31:15 -0800

    react router implementation

--- app/index.js
+++ app/index.js
@@ -1,49 +1,8 @@
-var USER_DATA = {
-  name: 'Theresa Luu',
-  username: 'rubynista',
-  image: 'http://en.gravatar.com/userimage/49899440/a5f0e0d9d2f5d05e112678633aa21a64.jpg?size=200'
-}
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style ={{height: 100, width: 100}}></img>
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    );
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-})
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
